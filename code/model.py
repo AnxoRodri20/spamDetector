2a945fc2c40da39ca033348d1337909a17259bd0
AnxoRodri20 <AnxoRodri20@gmail.com>
2025-02-10 19:31:53 +0100

    detector raw

---
+++ code/model.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import joblib
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+
+df = pd.read_csv('archive/spam.csv', encoding='latin-1')
+df = df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1)
+df = df.rename(columns={'v1': 'label', 'v2': 'text'})
+
+
+df["text"] = df["text"].str.lower()
+df["text"] = df["text"].apply(word_tokenize)
+stop_words = set(stopwords.words("english"))
+df["text"] = df["text"].apply(lambda x: [word for word in x if word not in stop_words])
+df["text"] = df["text"].apply(lambda x: " ".join(x))
+
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df["text"])
+
+X_train, X_test, y_train, y_test = train_test_split(X, df["label"], test_size=0.2, random_state=42)
+
+classifier = MultinomialNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+print("Accuracy:", accuracy)
+print("Classification Report:")
+print(report)
+
+joblib.dump(classifier, 'model/sms_spam_model.pkl')
+joblib.dump(vectorizer, 'model/sms_spam_vectorizer.pkl')
